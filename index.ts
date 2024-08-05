34342a69d1d3810e18d24eb737eeac68fcfc2133
Bengt Brodersen <bengt.brodersen@gmail.com>
2024-08-05 17:01:10 +0200

    init

--- index.ts
+++ index.ts
@@ -1,48 +1,80 @@
+/* eslint-disable camelcase */
 import * as core from '@actions/core'
-import * as github from '@actions/github'
-import {HttpClient} from '@actions/http-client'
-import {exec, getInput, getYamlInput, run} from './lib/actions.js'
-// see https://github.com/actions/toolkit for more github actions libraries
-import {z} from 'zod'
+import {getInput} from '@actions/core'
+import {run} from './lib/actions.js'
 import {fileURLToPath} from 'url'
+import * as process from 'node:process'
 
 export const action = () => run(async () => {
-  const context = github.context
   const inputs = {
-    token: getInput('token', {required: true})!,
-    string: getInput('stringInput'),
-    yaml: z.optional(z.array(z.string())).default([])
-        .parse(getYamlInput('yamlInput')),
+    vars: getInput('vars') ? JSON.parse(getInput('vars')) || {} : {},
+    secrets: getInput('secrets') ? JSON.parse(getInput('secrets')) || {} : {},
+    scope: getInput('scope'),
+    export: getInput('export')
+        // split by newline or comma
+        .split(/[\n,]/)
+        // trim whitespace
+        .map((name) => name.trim())
+        // remove empty strings
+        .filter((name) => name !== ''),
   }
-  const octokit = github.getOctokit(inputs.token)
 
-  await octokit.rest.issues.create({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    title: 'New issue',
-    body: 'This is a new issue',
+  core.setOutput('scope', inputs.scope)
+
+  inputs.export.forEach((exportName) => {
+    let sourceName: string | undefined
+    let sourceValue: string | undefined
+
+    if (inputs.scope) {
+      // postfix scope
+      const postfixScopedName = `${exportName}__${inputs.scope}`
+      const postfixScopedValue = getContextValue(postfixScopedName)
+      // prefix scope
+      const prefixScopedName = `${inputs.scope}__${exportName}`
+      const prefixScopedValue = getContextValue(prefixScopedName)
+
+      if (postfixScopedValue !== undefined && prefixScopedValue !== undefined) {
+        throw new Error(`Duplicated variable '${exportName}' for scope '${inputs.scope}' ` +
+            `(${postfixScopedValue}, ${prefixScopedValue}).`)
+      }
+
+      if (postfixScopedValue !== undefined) {
+        sourceName = postfixScopedName
+        sourceValue = postfixScopedValue
+      } else if (prefixScopedValue !== undefined) {
+        sourceName = prefixScopedName
+        sourceValue = prefixScopedValue
+      }
+    }
+
+    if (sourceValue === undefined) {
+      sourceName = exportName
+      sourceValue = getContextValue(exportName)
+    }
+
+    if (sourceValue === undefined) {
+      throw new Error(`Export name '${exportName}' could not be found in context.`)
+    }
+
+    if (sourceName === exportName) {
+      core.info(`Export variable '${exportName}'`)
+    } else {
+      core.info(`Export variable '${sourceName}' as '${exportName}'`)
+    }
+    core.exportVariable(exportName, sourceValue)
   })
 
-  const httpClient = new HttpClient()
-  httpClient.get('https://api.github.com').then((response) => {
-    core.info(`HTTP response: ${response.message.statusCode}`)
-  })
-
-  const result = await exec('echo', ['Hello world!'])
-      .then(({stdout}) => stdout.toString())
-
-  // core.setSecret(value) will mask the value in logs
-  core.setSecret('secretXXX')
-  core.info(result)
-
-  core.startGroup('Group title')
-  core.info(result)
-  core.endGroup()
-
-  // core.setFailed('This is a failure')
-  // core.setOutput(key,value) will set the value of an output
-  core.setOutput('stringOutput', result)
+  /**
+   * Get the value of a context variable
+   * @param name - variable name
+   * @returns the variable value
+   */
+  function getContextValue(name: string) {
+    return inputs.secrets[name] ?? inputs.vars[name]
+  }
 })
+
+// --- main ---
 
 // Execute the action, if running as the main module
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
